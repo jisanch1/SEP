0983bffa2102fb973dc5e75c5164e2ae78b952c3
Javier Sánchez <jisanch1@gmail.com>
2016-04-17 20:35:38 -0300

    lab 7

--- lab7/lab.c
+++ lab7/lab.c
@@ -1,14 +1,15 @@
 #define F_CPU   16000000UL                  // clk set to 16MHz 
-#define DELAY   10
 #define TRIG    OCR0A 
 #include <avr/io.h> 
 #include <util/delay.h> 
 
 uint8_t adc_read(void);
-void delay(void);
+uint8_t value;
+void set_leds(uint8_t value);
 
 void init(void)
 {
+    DDRD = 0x7F;                                // Enable first 7 pins to output.
     DDRD |= (1 << DDD6);                        // Enable pin OC0A to output. (PD6)
     TCCR0A |= (1 << COM0A1);                    // Compare output mode: up at 0x00, down on match with OCR0A.
     TCCR0A |= (1 << WGM01) | (1 << WGM00);      // Waveform generation mode: Fast PWM, count to top.
@@ -19,12 +20,16 @@ void init(void)
     ADCSRA |= _BV(ADEN);                        // Enable ADC.
     ADMUX = 0x00;                               // ADC0 as input. (PC0)
     ADMUX |= _BV(REFS0);                        // AVcc at Vmax.
-    ADMUX |= _BV(ADLAR);                        // Use the ADC High byte.        
+    ADMUX |= _BV(ADLAR);                        // Use the ADC High byte.
+
+    value = 0x00;        
 }
 
 void run(void)
 {
-    TRIG = adc_read();
+    value = adc_read();
+    TRIG = (value >> 2);
+    set_leds(value);
 }
  
 int main (void) 
@@ -45,3 +50,31 @@ uint8_t adc_read(void)
 
     return ADCH;                                // Read most significative pins.
 }
+
+void set_leds(uint8_t m)
+{
+    static uint8_t n = 0;
+    if (m != n)
+    {
+        PORTD &= ~0x3F;
+        if (m < 32)
+            PORTD |= 0x00 & 0x3F;
+        else if (m >= 32 && m < 64)
+            PORTD |= 0x01 & 0x3F;
+        else if (m >= 64 && m < 3*32)
+            PORTD |= 0x03 & 0x3F;
+        else if (m >= 3*32 && m < 4*32)
+            PORTD |= 0x07 & 0x3F;
+        else if (m >= 4*32 && m < 5*32)
+            PORTD |= 0x0F & 0x3F;
+        else if (m >= 5*32 && m < 6*32)
+            PORTD |= 0x1F & 0x3F;
+        else if (m >= 6*32)
+            PORTD |= 0x3F & 0x3F;
+    }
+    n = m;
+}
+
+
+
+
